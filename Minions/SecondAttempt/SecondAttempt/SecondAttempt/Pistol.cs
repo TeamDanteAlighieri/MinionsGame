e75bf375c6b0a6e2877a5f59f273c7f19f273537
Yoanna Mareva <jo.thirdeyevision@gmail.com>
2014-02-15 02:28:57 +0200

    New classes added
    Item, gun, etc.

---
+++ Minions/SecondAttempt/SecondAttempt/SecondAttempt/Pistol.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecondAttempt
+{
+    class Pistol : Gun
+    {
+        private const int addSpeed = 30;
+
+
+        public Pistol(int attackPower, int speed)
+            : base(attackPower, speed)
+        {
+            base.Speed += addSpeed;
+        }
+
+
+    }
+}
