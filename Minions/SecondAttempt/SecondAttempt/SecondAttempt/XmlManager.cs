c5cda7b59f6566b5c4cc3498ff920f4b340448ad
epitsin <epitsin@yahoo.com>
2014-02-25 15:19:01 +0200

    No saved game to load - Exception class added

--- Minions/SecondAttempt/SecondAttempt/SecondAttempt/XmlManager.cs
+++ Minions/SecondAttempt/SecondAttempt/SecondAttempt/XmlManager.cs
@@ -1,42 +1,45 @@
 ﻿
 namespace SecondAttempt
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
 
-    using System.Xml.Serialization;
-    using System.IO;
+	using System.Xml.Serialization;
+	using System.IO;
 
-    public class XmlManager<T>
-    {
-        public Type Type;
+	public class XmlManager<T>
+	{
+		public Type Type;
 
-        public XmlManager()
-        {
-            Type = typeof(T);
-        }
+		public XmlManager()
+		{
+			Type = typeof(T);
+		}
 
-        public T Load(string path)
-        {
-            T instance;
+		public T Load(string path)
+		{
+			T instance;
+			try
+			{
+				using (TextReader reader = new StreamReader(path))
+				{
+					XmlSerializer xml = new XmlSerializer(Type);
+					instance = (T)xml.Deserialize(reader);
+				}
+				return instance;
+			}
+			catch { throw new LoadGameException("You don't have any saved games to load!"); }
+		}
 
-            using (TextReader reader = new StreamReader(path))
-            {
-                XmlSerializer xml = new XmlSerializer(Type);
-                instance = (T)xml.Deserialize(reader);
-            }
-            return instance;
-        }
-
-        public void Save(String path, object obj)
-        {
-            using (TextWriter writer = new StreamWriter(path))
-            {
-                XmlSerializer xml = new XmlSerializer(Type);
-                xml.Serialize(writer, obj);
-            }
-        }
-    }
+		public void Save(String path, object obj)
+		{
+			using (TextWriter writer = new StreamWriter(path))
+			{
+				XmlSerializer xml = new XmlSerializer(Type);
+				xml.Serialize(writer, obj);
+			}
+		}
+	}
 }
