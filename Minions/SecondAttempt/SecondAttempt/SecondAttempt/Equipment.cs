86acaad06e2116c229214af3d90e1032b5702a70
IvayloGeorgiev <georgievir@yahoo.com>
2014-02-25 15:30:12 +0000

    Equipment class added. Added a method to the IStats interface.

---
+++ Minions/SecondAttempt/SecondAttempt/SecondAttempt/Equipment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SecondAttempt
+{
+    public class Equipment : Item, IEquipable
+    {
+        public bool IsEquiped;
+
+
+        public void Equip(Character target)
+        {
+            if (!IsEquiped)
+            {
+                
+            }
+        }
+    }
+}
