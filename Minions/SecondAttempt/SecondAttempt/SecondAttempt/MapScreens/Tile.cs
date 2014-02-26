cb65057c94fd5c5a7b43baa86e3e167a3ce98381
jasonbashov <jbashov@gmail.com>
2014-02-26 16:09:42 +0200

    Splashscreen changed

--- Minions/SecondAttempt/SecondAttempt/SecondAttempt/MapScreens/Tile.cs
+++ Minions/SecondAttempt/SecondAttempt/SecondAttempt/MapScreens/Tile.cs
@@ -15,6 +15,7 @@ namespace SecondAttempt
         private Rectangle sourceRect;
         private string state;
         private bool isShopkeeper;
+        private event EventHandler OpenShop;
 
         public Rectangle SourceRect
         {
@@ -24,6 +25,12 @@ namespace SecondAttempt
         public Vector2 Position
         {
             get { return position; }
+        }
+
+        protected virtual void OnShopTile(EventArgs e)
+        {
+            if (OpenShop != null)
+                OpenShop(this, e);
         }
 
         public void LoadContent(Vector2 position, Rectangle sourceRect, string state, bool isShopkeeper)
